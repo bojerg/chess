4b7d226a943e95350d342f668bbbe85b80c65f6c
bojerg <40041321+bojerg@users.noreply.github.com>
2023-01-13 04:25:15 -0600

    first actual commit for real

--- board.go
+++ board.go
@@ -1,1 +1,38 @@
-package chess
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+)
+
+type Board struct {
+}
+
+func (b *Board) Draw(boardImage *ebiten.Image) {
+	tileSize := 128
+	tileImage := ebiten.NewImage(tileSize, tileSize)
+	// Row, Column
+	for r := 0; r < 8; r++ {
+		for c := 0; c < 8; c++ {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64(c*tileSize+448), float64(r*tileSize+28))
+
+			//determine if tile is light or dark
+			if r%2 == 0 {
+				if c%2 == 0 {
+					tileImage.Fill(color.White)
+				} else {
+					tileImage.Fill(color.Black)
+				}
+			} else {
+				if c%2 == 0 {
+					tileImage.Fill(color.Black)
+				} else {
+					tileImage.Fill(color.White)
+				}
+			}
+
+			boardImage.DrawImage(tileImage, op)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,68 @@
-package chess
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"log"
+)
+
+// Game implements ebiten.Game interface.
+type Game struct {
+	board      *Board
+	boardImage *ebiten.Image
+}
+
+// Update proceeds the game state.
+// Update is called every tick (1/60 [s] by default).
+func (g *Game) Update() error {
+	// Write your game's logical update.
+	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+		ebiten.SetFullscreen(false)
+	}
+
+	return nil
+}
+
+// Draw draws the game screen.
+// Draw is called every frame (typically 1/60[s] for 60Hz display).
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Write your game's rendering.
+	if g.boardImage == nil {
+		g.boardImage = ebiten.NewImage(1920, 1080)
+	}
+	//screen.Fill(backgroundColor)
+	g.board.Draw(g.boardImage)
+	op := &ebiten.DrawImageOptions{}
+	sw, sh := screen.Size()
+	bw, bh := g.boardImage.Size()
+	x := (sw - bw) / 2
+	y := (sh - bh) / 2
+	op.GeoM.Translate(float64(x), float64(y))
+	screen.DrawImage(g.boardImage, op)
+}
+
+// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
+// If you don't have to adjust the screen size with the outside size, just return a fixed size.
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	/*
+		w := screenWidth / 2
+		h := screenHeight / 2
+		if w < 1 || h < 1 {
+			w = 1
+			h = 1
+		}
+		return w, h
+	*/
+	return 1920, 1080
+}
+
+func main() {
+	game := &Game{}
+	// Specify the window size as you like. Here, a doubled size is specified.
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("chess")
+	ebiten.SetFullscreen(true)
+	// Call ebiten.RunGame to start your game loop.
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
